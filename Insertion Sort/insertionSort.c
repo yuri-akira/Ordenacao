bb70b22c5b59e1636d76608164e2b2a9660e5b5c
Yuri Akira Shiba <61916114+yuri-akira@users.noreply.github.com>
2020-03-12 01:08:55 -0300

    Create insertionSort.c

---
+++ Insertion Sort/insertionSort.c
@@ -0,0 +1,15 @@
+void insertionSort(int vetor[],int vetorTam)
+{   
+    int atual,j;
+
+    for(int i = 1; i < vetorTam; i++)
+    {
+        atual = vetor[i];
+        j = i - 1;
+        while(j >= 0 && atual < j)
+        {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+    }
+}
